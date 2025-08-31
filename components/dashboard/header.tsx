c7f9d4249602f110820cad1a7ab1b6096c38e07b
babuas25 <babuas25@gmail.com>
2025-08-31 13:53:42 +0600

    Fix missing Link import in header component

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import { Search, LogOut, User } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
